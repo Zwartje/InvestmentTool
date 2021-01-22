98e06214014d358d00096ea1ffbb90a0d24feebe
HBao <bhl.zju@gmail.com>
2021-01-22 18:38:23 +0100

    Commit test.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def print_hi(name):
     :param name:
     :return:
     """
+    # test
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
