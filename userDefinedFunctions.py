ab0956b893ce84b173939ad783ff2beb7a8aa8b2
HBao <bhl.zju@gmail.com>
2021-03-12 17:13:55 +0100

    Update docstrings to initiate documentation.

---
+++ userDefinedFunctions.py
@@ -0,0 +1,8 @@
+def my_function(function_name):
+    """
+
+    :param function_name: test docstring
+    :return: test docstring
+    """
+    test = function_name + function_name
+    return(test)
