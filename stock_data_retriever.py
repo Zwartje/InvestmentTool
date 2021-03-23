05bd85726b922726e74637838884e56ad438a64b
HBao <bhl.zju@gmail.com>
2021-03-23 22:44:17 +0100

    Update stock_data_retriever.py and create the demo of py gui.

--- stock_data_retriever.py
+++ stock_data_retriever.py
@@ -1,6 +1,26 @@
 import yfinance as yf
+import PySimpleGUI as sg
+import matplotlib.pyplot as plt
 
-stock_ticker = yf.Ticker("AAPL")
-hist = stock_ticker.history(period="max")
-hist['Close'].plot(title="Apple's stock price")
 
+def draw_plot(ticker_name):
+    stock_ticker = yf.Ticker(ticker_name)
+    hist = stock_ticker.history(period="max")
+    plt.plot(hist['Close'])
+    plt.show(block=False)
+
+
+#    plt.show(block=False)
+
+layout = [[sg.Text('Stock name:'), sg.Input(key='IN')],
+          [sg.Button('Plot'), sg.Cancel()]]
+
+window = sg.Window('Have some Matplotlib....', layout)
+
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Cancel'):
+        break
+    elif event == 'Plot':
+        draw_plot(values['IN'])
+window.close()
